cfd1e39c1dae2a0a3cdea20c799f0d6083d9cb5b
Umberto <umbertoo032@gmail.com>
2022-03-31 16:56:04 +0200

    prima prova

---
+++ src/routes/admin/prova.svelte
@@ -0,0 +1,12 @@
+<div>
+
+    <button type="button" class="btn btn-primary" on:click={funzione}>Primary</button>
+</div>
+
+<script>
+import { goto } from "$app/navigation";
+
+    function funzione(){
+        goto("https://getbootstrap.com/docs/5.1/components/buttons/")
+    }
+</script>
