67d0fd3aaad623761e788a061017a1a510022dec
Erika <sciaccaerika0@gmail.com>
2022-03-31 16:19:24 +0200

    nuove modifiche

---
+++ src/routes/public/__layout.svelte
@@ -0,0 +1,11 @@
+<div class="container-fluid">
+	<div>Ciao sono il layout</div>
+
+	<div class="container">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss" global>
+	@import 'bootstrap/scss/bootstrap';
+</style>
