3376b50f9e7f280a0f4d3c0085e7f0c1d225d670
Umberto <umbertoo032@gmail.com>
2022-04-01 20:46:36 +0200

    fine topnav

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="https://getbootstrap.com/docs/5.1/components/navbar/">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+  </nav>
+
+  <style lang="scss" global>
+	@import 'bootstrap/scss/bootstrap';
+</style>
