b045711a6558f064f2a07b53178ec0b8ed49f841
Erika <sciaccaerika0@gmail.com>
2022-04-01 17:54:43 +0200

    fatto footer e topnav

--- src/routes/public/__layout.svelte
+++ src/routes/public/__layout.svelte
@@ -1,8 +1,67 @@
 <div class="container-fluid">
-	<div>Ciao sono il layout</div>
-
+	<div class="p-3 mb-2 bg-warning text-dark">
+		<ul class="nav justify-content-end">
+			<li class="nav-item">
+				<a
+					class="nav-link active"
+					aria-current="page"
+					href="https://challonge.com/it/dashboard.html">Accedi</a
+				>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://challonge.com/it/dashboard.html">Registrati</a>
+			</li>
+		</ul>
+	</div>
 	<div class="container">
 		<slot />
+	</div>
+
+	<div class="container">
+		
+		<footer class="row row-cols-5 py-5 my-5 border-top">
+			<div class="col">
+				<a href="" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+					<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+				</a>
+				<p class="text-muted">© 2022</p>
+			</div>
+
+			<div class="col" />
+
+			<div class="col">
+				<h5>Section</h5>
+				<ul class="nav flex-column">
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+				</ul>
+			</div>
+
+			<div class="col">
+				<h5>Section</h5>
+				<ul class="nav flex-column">
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+				</ul>
+			</div>
+
+			<div class="col">
+				<h5>Section</h5>
+				<ul class="nav flex-column">
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+				</ul>
+			</div>
+		</footer>
 	</div>
 </div>
 
