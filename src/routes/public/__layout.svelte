655e4d345fa660e0e5da0a4d6ca0de335a40a564
Erika <sciaccaerika0@gmail.com>
2022-04-04 16:15:47 +0200

    Nuove modifche

--- src/routes/public/__layout.svelte
+++ src/routes/public/__layout.svelte
@@ -1,23 +1,40 @@
+	
 <div class="container-fluid">
-	<div class="p-3 mb-2 bg-warning text-dark">
-		<ul class="nav justify-content-end">
-			<li class="nav-item">
-				<a
-					class="nav-link active"
-					aria-current="page"
-					href="https://challonge.com/it/dashboard.html">Accedi</a
-				>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="https://challonge.com/it/dashboard.html">Registrati</a>
-			</li>
-		</ul>
-	</div>
-	<div class="container">
-		<slot />
-	</div>
+	<!-- Navbar content -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light" >
 
-	<div class="container">
+			<a class="navbar-brand" href="#">
+				<img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+				NomeSito
+			  </a>
+		 <!-- <a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>-->
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+			  <li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="#">Home</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Accedi</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Registrati</a>
+			  </li>
+			</ul>
+			<form class="d-flex">
+			  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+			  <button class="btn btn-outline-success" type="submit">Search</button>
+			</form>
+		  </div>
+		
+	  </nav>
+</div>
+<div class="container">
+		<slot />
+
+	
 		
 		<footer class="row row-cols-5 py-5 my-5 border-top">
 			<div class="col">
@@ -63,8 +80,13 @@
 			</div>
 		</footer>
 	</div>
-</div>
 
 <style lang="scss" global>
 	@import 'bootstrap/scss/bootstrap';
+
+	.ratio-4x3 {
+  @include media-breakpoint-up(md) {
+    --bs-aspect-ratio: 50%; // 2x1
+  }
+}
 </style>
--- src/routes/public/ciao.svelte
+++ src/routes/public/ciao.svelte
@@ -1,1 +1,34 @@
-igtgsw
+<div class="p-3 mb-2 bg-secondary text-white">
+	<div class="row align-items-start">
+		<div class="col">
+			<div class="container">
+				<center>
+					<h1>Affidati a noi per gestire il tuo torneo!!</h1>
+					<br /><br /><br />
+					<h3>
+						Basta più calcoli, con noi potrai organizzare il tuo torneo facilmente e gestirlo
+						dinamicamente.
+					</h3>
+					<br /><br /><br /><br /></center
+				>
+
+				<div class="d-grid gap-2 col-6 mx-auto">
+					<button type="button" class="btn btn-outline-warning"><h1>CREA IL TUO TORNEO</h1></button>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<img
+				src="https://thumbs.dreamstime.com/b/sostegno-di-torneo-schema-campionato-calcio-con-la-tazza-del-trofeo-vettore-sport-108276770.jpg"
+				width="80%"
+				height="80%"
+				class="rounded mx-auto d-block"
+				alt="..."
+			/>
+		</div>
+	</div>
+</div>
+
+<div class="ratio ratio-4x3">
+    <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+  </div>
