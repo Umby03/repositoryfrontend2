7a7c1717eb6420f2ccb4a5f489d62c41f9c85ec6
Erika <sciaccaerika0@gmail.com>
2022-04-05 16:04:02 +0200

    nuove modifiche

---
+++ src/components/FooterComponents.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+
+export let page_title:string
+
+</script>
+
+
+<footer class="row row-cols-5 py-5 my-5 border-top">
+
+<div>
+
+    titolo della pagina {page_title}
+
+</div>
+
+    <div class="col">
+        <a href="" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+        </a>
+        <p class="text-muted">© 2022</p>
+    </div>
+
+    <div class="col" />
+
+    <div class="col">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+    </div>
+
+    <div class="col">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+    </div>
+
+    <div class="col">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+    </div>
+</footer>
--- src/routes/public/__layout.svelte
+++ src/routes/public/__layout.svelte
@@ -1,87 +1,71 @@
-	
+<script lang="ts">
+import { goto } from '$app/navigation';
+
+
+import FooterComponent from '../../components/FooterComponents.svelte'
+let ciaovar=2*4
+
+$:{
+	if(!ciaovar) ciaovar=0
+}
+
+
+function actionClick(){
+
+	ciaovar=ciaovar*2
+	goto('/admin/prova')
+}
+
+</script>
+
 <div class="container-fluid">
 	<!-- Navbar content -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light" >
-
-			<a class="navbar-brand" href="#">
-				<img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-			  </a>
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">
+			<img
+				src="/docs/5.1/assets/brand/bootstrap-logo.svg"
+				alt=""
+				width="30"
+				height="24"
+				class="d-inline-block align-text-top"
+			/>
+		</a>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-			  <li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="#"><h3>Home</h3></a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#"><h3>Accedi</h3></a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#"><h3>Registrati</h3></a>
-			  </li>
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#"><h3>Home</h3></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><h3>Accedi</h3></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><h3>Registrati</h3></a>
+				</li>
 			</ul>
 			<form class="d-flex">
-			  <input class="form-control me-2" type="search" placeholder="Cerca un Torneo" aria-label="Search">
-			  <button class="btn btn-outline-success" type="submit">Search</button>
+				<input
+					class="form-control me-2"
+					type="search"
+					placeholder="Cerca un Torneo"
+					aria-label="Search"
+					style="font-size:19px;"
+				/>
+				<button class="btn btn-outline-success" type="submit" style="font-size:19px;">Search</button
+				>
 			</form>
-		  </div>
-		
-	  </nav>
+		</div>
+	</nav>
 </div>
-<br><br><br><br><br><br>
+<br /><br /><br /><br /><br /><br />
 <div class="container">
-		<slot />
+	<slot />
+{ciaovar}
 
-		
-		<footer class="row row-cols-5 py-5 my-5 border-top">
-			<div class="col">
-				<a href="" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-					<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
-				</a>
-				<p class="text-muted">© 2022</p>
-			</div>
+<button class="btn btn-primary" on:click={()=>{actionClick()}}>prova bottone</button>
 
-			<div class="col" />
-
-			<div class="col">
-				<h5>Section</h5>
-				<ul class="nav flex-column">
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-				</ul>
-			</div>
-
-			<div class="col">
-				<h5>Section</h5>
-				<ul class="nav flex-column">
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-				</ul>
-			</div>
-
-			<div class="col">
-				<h5>Section</h5>
-				<ul class="nav flex-column">
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-					<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-				</ul>
-			</div>
-		</footer>
-	</div>
+	<FooterComponent page_title="pagina0" />
+</div>
 
 <style lang="scss" global>
 	@import 'bootstrap/scss/bootstrap';
-
-	.ratio-4x3 {
-  @include media-breakpoint-up(md) {
-    --bs-aspect-ratio: 40%; // 2x1
-  }
-}
 </style>
