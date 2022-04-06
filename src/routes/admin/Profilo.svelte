96903273d784a57d554f96cb0fb4c055502cbee4
Umberto <umbertoo032@gmail.com>
2022-04-06 22:32:07 +0200

    pagina profilo

---
+++ src/routes/admin/Profilo.svelte
@@ -0,0 +1,19 @@
+<br/><br/><br/><br/>
+<div class="container">
+    <center>
+        <div class="card" style="width: 18rem">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h1 class="card-title">Username</h1>
+              <h2 class="card-text"><h2>Some quick example text to build on the card title and make up the bulk of the card's content.</h2>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+    </center>
+</div>
+
+<style lang="scss" global>
+div{
+    width: 100%;
+}
+</style>
