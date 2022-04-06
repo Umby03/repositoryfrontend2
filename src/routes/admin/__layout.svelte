28e7797afa3df72f62baa8d3ae4376793f221cf4
Umberto <umbertoo032@gmail.com>
2022-04-06 16:11:42 +0200

    inizializzazione file profilo

--- src/routes/admin/__layout.svelte
+++ src/routes/admin/__layout.svelte
@@ -1,5 +1,5 @@
 	<nav class="bg-light">
-		<a class="navbar-brand" href="#">Profilo</a>
+		<a class="navbar-brand" on:click={()=>{actionClick()}}><h2>Profilo</h2></a>
     <a class="brand" style="margin: 0; float: none;" href="#">
       <img
         src="/docs/5.1/assets/brand/bootstrap-logo.svg"
@@ -16,8 +16,10 @@
 				type="search"
 				placeholder="Cerca un Torneo"
 				aria-label="Search"
+				style="font-size:19px;"
+
 			/>
-			<button class="btn btn-outline-success" type="submit">Cerca</button>
+			<button class="btn btn-outline-success" type="submit" style="font-size:17px;">Cerca</button>
 		</form>
 	</nav>
 
@@ -25,51 +27,9 @@
 
 	<slot />
 
+
 	<br /><br /><br /><br /><br /><br />
-
-	<footer class="row row-cols-5 py-5 my-5 border-top">
-		<div class="col">
-			<a href="" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-				<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
-			</a>
-			<p class="text-muted">© 2022</p>
-		</div>
-
-		<div class="col" />
-
-		<div class="col">
-			<h5>Section</h5>
-			<ul class="nav flex-column">
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-			</ul>
-		</div>
-
-		<div class="col">
-			<h5>Section</h5>
-			<ul class="nav flex-column">
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-			</ul>
-		</div>
-
-		<div class="col">
-			<h5>Section</h5>
-			<ul class="nav flex-column">
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-				<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-			</ul>
-		</div>
-	</footer>
+<FooterComponent  />
 </div>
 
 <style lang="scss" global>
@@ -86,3 +46,13 @@
     width: 100%;
   }
 </style>
+
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import FooterComponent from '../../components/FooterComponents.svelte'
+
+
+function actionClick(){
+goto('/admin/Profilo')
+}
+</script>
--- src/routes/admin/primo.svelte
+++ src/routes/admin/primo.svelte
@@ -1,1 +1,28 @@
-ciao
+<br><br><br>
+<div class="row align-items-start">
+    <div class="col">
+        <div class="container">
+            <center>
+                <h1>Bentornato! Quale sarà la tua prossima mossa?</h1>
+                <br /><br /><br />
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button type="button" class="btn btn-outline-warning"><h1>Crea un torneo</h1></button>
+                </div>
+                <br /><br /><br /></center
+            >
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button type="button" class="btn btn-outline-warning"><h1>Visualizza Club</h1></button>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <img
+            src="https://thumbs.dreamstime.com/b/sostegno-di-torneo-schema-campionato-calcio-con-la-tazza-del-trofeo-vettore-sport-108276770.jpg"
+            width="80%"
+            height="80%"
+            class="rounded mx-auto d-block"
+            alt="schema torneo"
+        />
+    </div>
+    </div>
+
