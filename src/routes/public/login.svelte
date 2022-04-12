451a5f433210a4f93e8c1521923fd47449759c19
Erika <sciaccaerika0@gmail.com>
2022-04-12 17:13:13 +0200

    modifiche login e registrati

--- src/routes/public/login.svelte
+++ src/routes/public/login.svelte
@@ -1,62 +1,72 @@
+<script lang="ts">
+	let hasAccount = true;
+	const toggleHasAccount = () => {
+		hasAccount = !hasAccount;
+	};
+
+
+
+	import LoginComponent from '../../components/loginComponent.svelte'
+    import SignUpComponent from '../../components/signUpComponent.svelte'
+	/*function doRequestWithPromise() {
+		const url = 'http://192.168.46.55:8080/ping';
+
+		fetch(url)
+			.then((result) => {
+				result
+					.json()
+					.then((formattedJson) => {
+						console.log(formattedJson);
+
+					})
+					.catch((err) => {
+						console.error(err);
+					})
+					.finally(() => {
+						console.info('done');
+					});
+			})
+			.catch((err) => {
+				console.error(err);
+			})
+			.finally(() => {
+				console.info('done');
+			});
+	}
+	async function doRequest() {
+		try{
+			const url = 'http://192.168.46.55:8080/ping';
+
+const ciao = await fetch(url);
+const formattedJson = await ciao.json();
+console.log(formattedJson);
+		} catch(err){
+			console.error(err);
+
+		} finally{
+			console.info('done');
+
+		}
+
+	}
+
+	doRequest();
+
+	doRequestWithPromise();*/
+</script>
+
 <div class="container">
 	<div class="row align-items-start">
 		<div class="col" />
 
 		<div class="col">
-			<h1>Accedi</h1>
-			<br />
-			<form style="width:300px;">
-				<div class="mb-3">
-					<label for="exampleInputEmail1" class="form-label"><h3>Email address</h3></label>
-					<input
-						type="email"
-						class="form-control"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-						style="wight:20px;"
-					/>
-					<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-				</div>
-				<div class="mb-3">
-					<label for="exampleInputPassword1" class="form-label"><h3>Password</h3></label>
-					<input type="password" class="form-control" id="exampleInputPassword1" />
-				</div>
-				<div class="mb-3 form-check">
-					<input type="checkbox" class="form-check-input" id="exampleCheck1" />
-					<label class="form-check-label" for="exampleCheck1">Check me out</label>
-				</div>
-				<button type="button" class="btn btn-outline-warning"><h3>Submit</h3></button>
-			</form>
-			<br /><br /><br />
-			<h5>Non hai ancora un account?  </h5>
-			<h1>Registrati Adesso</h1>
-			<br />
-			<form style="width:300px;">
-				<div class="mb-3">
-					<label for="exampleInputEmail1" class="form-label"><h3>Email address</h3></label>
-					<input
-						type="email"
-						class="form-control"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-						style="wight:20px;"
-					/>
-					<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-				</div>
-				<div class="mb-3">
-					<label for="exampleInputPassword1" class="form-label"><h3>Nuova Password</h3></label>
-					<input type="password" class="form-control" id="exampleInputPassword1" />
-				</div>
-				<div class="mb-3">
-					<label for="exampleInputPassword1" class="form-label"><h3>Ripeti Password</h3></label>
-					<input type="password" class="form-control" id="exampleInputPassword1" />
-				</div>
-				<div class="mb-3 form-check">
-					<input type="checkbox" class="form-check-input" id="exampleCheck1" />
-					<label class="form-check-label" for="exampleCheck1">Check me out</label>
-				</div>
-				<button type="button" class="btn btn-outline-warning"><h3>Submit</h3></button>
-			</form>
+			{#if hasAccount}
+
+			<LoginComponent toggleHasAccount={toggleHasAccount} />
+			{:else}
+
+			<SignUpComponent toggleHasAccount={toggleHasAccount} />
+				{/if}
 		</div>
 
 		<div class="col" />
