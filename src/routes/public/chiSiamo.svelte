9d47926b27e2f46b294e9e2ac6b72b24664a64d6
Erika <sciaccaerika0@gmail.com>
2022-04-19 13:28:57 +0200

    nuove modifche

--- src/routes/public/chiSiamo.svelte
+++ src/routes/public/chiSiamo.svelte
@@ -1,115 +1,197 @@
-    <center>
-      <br> <br> <br> <br>
-      <h1>Chi siamo? </h1>
-      <br> <br> 
-    <div class="containermio"style="padding-right: 35px;padding-left: 35px; text-" >
-
-      <h4>Fondato nel Maggio 2022, *nome* è un progetto ideato e sviluppato da una giovane community di ragazzi neo diplomati dell'Istituto Galileo Ferraris di Acireale.
-
-        L'idea è nata da un bisogno personale: volevamo organizzare un torneo tra amici e condividere i risultati online. Ecco come è nata *nome* per rendere i tornei semplici da gestire, più emozionanti e visibili!
-        
-        Da allora, *nome* è utilizzato da migliaia di utenti soddisfatti in tutto il mondo ( si ti piaceva)  per gestire tornei e leghe di qualsiasi sport o gioco.
-      </h4>
-   </div> 
-  </center>
 <center>
-  <br> <br> <br> <br>
-   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="width:60%">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg" class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+	<br /> <br /> <br /> <br />
+	<h1>Chi siamo?</h1>
+	<br /> <br />
+	<div class="containermio" style="padding-right: 35px;padding-left: 35px; text-">
+		<h4>
+			Fondato nel Maggio 2022, *nome* è un progetto ideato e sviluppato da una giovane community di
+			ragazzi neo diplomati dell'Istituto Galileo Ferraris di Acireale. L'idea è nata da un bisogno
+			personale: volevamo organizzare un torneo tra amici e condividere i risultati online. Ecco
+			come è nata *nome* per rendere i tornei semplici da gestire, più emozionanti e visibili! Da
+			allora, *nome* è utilizzato da migliaia di utenti soddisfatti in tutto il mondo ( si ti
+			piaceva) per gestire tornei e leghe di qualsiasi sport o gioco.
+		</h4>
+	</div>
+</center>
+<center>
+	<br /> <br /> <br /> <br />
+	<div
+		id="carouselExampleIndicators"
+		class="carousel slide"
+		data-bs-ride="carousel"
+		style="width:60%"
+	>
+		<div class="carousel-indicators">
+			<button
+				type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide-to="0"
+				class="active"
+				aria-current="true"
+				aria-label="Slide 1"
+			/>
+			<button
+				type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide-to="1"
+				aria-label="Slide 2"
+			/>
+			<button
+				type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide-to="2"
+				aria-label="Slide 3"
+			/>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img
+					src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg"
+					class="d-block w-100"
+					alt="..."
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg"
+					class="d-block w-100"
+					alt="..."
+				/>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="https://www.welfaregratis.it/wp-content/uploads/2019/11/shutterstock_1391807393-1200x675.jpg"
+					class="d-block w-100"
+					alt="..."
+				/>
+			</div>
+		</div>
+		<button
+			class="carousel-control-prev"
+			type="button"
+			data-bs-target="#carouselExampleIndicators"
+			data-bs-slide="prev"
+		>
+			<span class="carousel-control-prev-icon" aria-hidden="true" />
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button
+			class="carousel-control-next"
+			type="button"
+			data-bs-target="#carouselExampleIndicators"
+			data-bs-slide="next"
+		>
+			<span class="carousel-control-next-icon" aria-hidden="true" />
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
 </center>
 
-
-  <section class="contact-section section-3 mb0 custom-bg" data-bg="" style="padding: 85px 0px 70px;">
-		<div class="bg-layer"></div>
-		<div class="container">
-                    <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2">
-                            <h2 class="color-white mt10 mb30 text-center text-uppercase">RICHIEDI INFORMAZIONI</h2>
-                            <div class="content">
-                                <form class="working-contact-form form" method="POST" action="#" role="form" >
-                                    <!-- Form Fields -->
-                                    <div>
-                                        <div class="row">
-                                            <div class="col-sm-12 col-md-6">
-                                                <div class="form-group">
-                                                    <div class="form-icon icon-user">
-                                                        <input type="text" name="fullname" class="form-control" placeholder="Nome e cognome" aria-required="true">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12 col-md-6">
-                                                <div class="form-group">
-                                                    <div class="form-icon icon-company">
-                                                        <input type="text" name="companyname" class="form-control" placeholder="Organizzazione/Società" aria-required="true">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-sm-12 col-md-6">
-                                                <div class="form-group">
-                                                    <div class="form-icon icon-phone">
-                                                        <input type="text" name="phonenumber" class="form-control" placeholder="Numero di telefono"  aria-required="true">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12 col-md-6">
-                                                <div class="form-group">
-                                                    <div class="form-icon icon-email">
-                                                        <input type="email" name="email" class="form-control" placeholder="Indirizzo email"  aria-required="true">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-sm-12">
-                                                <div class="form-group">
-                                                    <div class="form-icon icon-message">
-                                                        <textarea name="note" class="form-control" cols="30" rows="8" placeholder="Messaggio..."  aria-required="true"></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                          <center>
-                                            <button type="submit" class="btn btn-outline-warning rounded" style="wight:30px;">Contattaci</button>
-                                          </center>
-                                        </div>
-                                    </div> <!-- END Form Fields -->
-                                    <!-- Success and error Alerts -->
-                                    <div class="alert alert-success hidden br0" id="contact-success">
-                                    <span class="glyphicon glyphicon-ok "></span> &nbsp;
-                                        <strong>Grazie per averci contattato!</strong> Sarai ricontattato al più presto da un nostro esperto.                                    </div>
-                                    <div class="alert alert-danger hidden br0" id="contact-error">
-                                        <span class="glyphicon glyphicon-remove "></span> &nbsp;
-                                        <strong>Errore!</strong> Qualcosa non ha funzionato. Riprova o chiama al +39 080.455.0031.                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- END Container -->
-            </section>
+<section class="contact-section section-3 mb0 custom-bg" data-bg="" style="padding: 85px 0px 70px;">
+	<div class="bg-layer" />
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2 class="color-white mt10 mb30 text-center text-uppercase">RICHIEDI INFORMAZIONI</h2>
+				<div class="content">
+					<form class="working-contact-form form" method="POST" action="#" role="form">
+						<!-- Form Fields -->
+						<div>
+							<div class="row">
+								<div class="col-sm-12 col-md-6">
+									<div class="form-group">
+										<div class="form-icon icon-user">
+											<input
+												type="text"
+												name="fullname"
+												class="form-control"
+												placeholder="Nome e cognome"
+												aria-required="true"
+											/>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-12 col-md-6">
+									<div class="form-group">
+										<div class="form-icon icon-company">
+											<input
+												type="text"
+												name="companyname"
+												class="form-control"
+												placeholder="Organizzazione/Società"
+												aria-required="true"
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12 col-md-6">
+									<div class="form-group">
+										<div class="form-icon icon-phone">
+											<input
+												type="text"
+												name="phonenumber"
+												class="form-control"
+												placeholder="Numero di telefono"
+												aria-required="true"
+											/>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-12 col-md-6">
+									<div class="form-group">
+										<div class="form-icon icon-email">
+											<input
+												type="email"
+												name="email"
+												class="form-control"
+												placeholder="Indirizzo email"
+												aria-required="true"
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="form-group">
+										<div class="form-icon icon-message">
+											<textarea
+												name="note"
+												class="form-control"
+												cols="30"
+												rows="8"
+												placeholder="Messaggio..."
+												aria-required="true"
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<center>
+									<button type="submit" class="btn btn-outline-warning rounded" style="wight:30px;"
+										>Contattaci</button
+									>
+								</center>
+							</div>
+						</div>
+						<!-- END Form Fields -->
+						<!-- Success and error Alerts -->
+						<div class="alert alert-success hidden br0" id="contact-success">
+							<span class="glyphicon glyphicon-ok " /> &nbsp;
+							<strong>Grazie per averci contattato!</strong> Sarai ricontattato al più presto da un nostro
+							esperto.
+						</div>
+						<div class="alert alert-danger hidden br0" id="contact-error">
+							<span class="glyphicon glyphicon-remove " /> &nbsp;
+							<strong>Errore!</strong> Qualcosa non ha funzionato. Riprova o chiama al +39 080.455.0031.
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END Container -->
+</section>
