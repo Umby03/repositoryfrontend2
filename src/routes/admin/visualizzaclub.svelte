00b6fd8ea78b8553dfc42654937a0e4b08217511
umberto <umbertoo032@gmai.com>
2022-05-25 16:13:25 +0200

    okok

---
+++ src/routes/admin/visualizzaclub.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+let results = [];
+
+$:{
+		if (typeof localStorage != 'undefined') {
+			fetch('http://192.168.114.55:8080/tournament/search', {
+				method: 'post',
+
+				headers: {
+					'content-type': 'application/json',
+					authorization: localStorage.getItem('token')
+				}
+			})
+				.then((resp) => resp.json())
+				.then((json) => {
+					results = json;
+				});
+		}
+	}
+
+</script>
